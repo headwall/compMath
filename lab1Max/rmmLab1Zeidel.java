42a1e1be8b43cff8de9d797c6ce0777c7b15c163
Яна Санникова < >
2016-09-22 19:39:22 +0500

    something about lab1

---
+++ lab1Max/rmmLab1Zeidel.java
@@ -0,0 +1,54 @@
+import java.math;
+import java.io.*;
+
+public class lab1Zeidel
+{
+	public static void main(String[] args) 
+}
+
+public class Matrix
+{
+	privat byte size;
+	privat float[][] matrix;
+
+	public getMatrix()
+	{
+		return matrix;
+	} 
+
+	public Matrix(byte size, float[][] matrix)
+	{
+		this.size = size;
+		this.matrix  matrix;
+	}
+
+	public getMatrixFromFile(String fileName)
+	{
+		File dataFile = new File(fileName);
+
+	}
+
+	public makeRandomMatrix(byte size)
+	{
+
+	}
+}
+
+public class Zeidel
+{
+	static float[][] result;
+	static float[] variableCol;
+	static float[] variableEpsCol;
+	static float eps;
+	static int iterationsAmount = 0;
+
+	public static float[][] solve(byte size, float[][] matrix, float eps)
+	{
+		
+	}
+
+	public static boolean diagDominationCheck(float[][] matrix) 
+	{
+
+	}
+}
